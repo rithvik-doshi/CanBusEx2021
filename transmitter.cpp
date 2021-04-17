a425dd3e70ed53aafdbc4f8818d10a087e3a1832
Rithvik Doshi <rithvik@bu.edu>
2021-04-17 15:19:47 -0400

    mostly working transmitter

--- transmitter.cpp
+++ transmitter.cpp
@@ -4,43 +4,45 @@
 #include <fstream>
 using std::cout;
 using std::string;
+using std::to_string;
 
-
-int main(int argc, char **argv){
-	int num;
-	try {
-		//cout << argv[1] << "\n";
-		num = atoi(argv[1]);
-	}
-	catch (...) {
-		cout << "Invalid input\n";
-		return 1;
-	}
-
-	//Decimal to Binary
-
+string inttobin(long int num){
 	string bin;
 	while (num > 0){
 		bin = (num%2==0?"0":"1")+bin;
 		num/=2;
 	}
+	return bin;
+}
+
+
+int main(int argc, char **argv){
+	
+	string bin = inttobin(atoi(argv[1]));
+	string len = inttobin(bin.size()/8);
+
+	while (len.size() < 4){
+		len = "0"+len;
+	}
 	//int binnum = stoi(bin);
 
 	// cout << binnum << "\n";
 
-	string SOF = "0"; //look up
-	string ID = "00100110010"
+	string StartOF = "0"; //Dominant Bit detected
+	string ID = "00100110010";
 	string RTR = "1";
-	string control = "000000"
-	string data = bin;
+	string control = ",00"+len+","; //length of data
+	string data = bin+",";
 	string CRC = "0000000000000000";
-	string ACK = "00";
-	string EOF = "0000000"
+	string ACK = "11"; //ACK Slot: transmitter sends recessive and receiver asserts dominant, ACK delimited always recessive;
+	string EndOF = "1111111";
 
-	string CAN_message = SOF+ID+RTR+control+data+CRC+ACK+EOF;
+	string CAN_message = StartOF+ID+RTR+control+data+CRC+ACK+EndOF;
 
-	std::ifstream file("can_buffer.txt");
-	
+	cout << CAN_message << "\n";
+
+	//std::ifstream file("can_buffer.txt");
+
 
 	return 0;
 }
